44db292a8c785bd8eb4bf00759565a782919efdb
David Stancu <dstancu@nyu.edu>
2020-11-29 16:58:53 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use cursive::views::{Dialog, TextView};
+
+fn main() {
+    let mut siv = cursive::default();
+
+    // Creates a dialog with a single "Quit" button
+    siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
+                         .title("Cursive")
+                         .button("Quit", |s| s.quit()));
+
+    // Starts the event loop.
+    siv.run();
+}
