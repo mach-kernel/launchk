2a8223cc38333322e317d6b6516345e98f343140
David Stancu <dstancu@nyu.edu>
2021-01-27 20:48:41 -0500

    as channel, but still no valid cxn

--- launchk/src/main.rs
+++ launchk/src/main.rs
@@ -1,7 +1,12 @@
+/*
+basic XPC message example
+dstancu@nyu.edu
+*/
+
 use cursive::views::{Dialog, TextView};
 
 use xpc_bindgen;
-use xpc_bindgen::{xpc_connection_create_mach_service, xpc_connection_t, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED, dispatch_queue_create, xpc_connection_set_event_handler, xpc_object_t, xpc_copy_description, xpc_int64_create, xpc_dictionary_create, xpc_connection_send_message, xpc_string_create, xpc_connection_resume};
+use xpc_bindgen::{xpc_connection_create_mach_service, xpc_connection_t, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED, dispatch_queue_create, xpc_connection_set_event_handler, xpc_object_t, xpc_copy_description, xpc_int64_create, xpc_dictionary_create, xpc_connection_send_message, xpc_string_create, xpc_connection_resume, XPC_CONNECTION_MACH_SERVICE_LISTENER, xpc_bool_create, xpc_connection_create};
 use std::ffi::CString;
 use std::os::raw::{c_char};
 use std::ptr::null_mut;
@@ -11,15 +16,22 @@ use cursive::view::AnyView;
 use std::any::Any;
 use std::collections::HashMap;
 use std::rc::Rc;
+use std::mem;
 
-static APP_ID: &str = "com.dstancu.fun";
+use std::sync::mpsc;
+use futures::channel::mpsc::unbounded;
+use futures::SinkExt;
+use std::thread::sleep;
+
+static APP_ID: &str = "com.dstancu.launchk";
 
 fn xpc_i64(value: i64) -> Rc<xpc_object_t> {
     unsafe {
         let new_obj = xpc_int64_create(value);
         let rc = Rc::new(new_obj);
-        let cloned = rc.clone();
-        cloned.to_owned()
+        rc
+        // let cloned = rc.clone();
+        // cloned.to_owned()
     }
 }
 
@@ -27,8 +39,19 @@ fn xpc_str(value: &str) -> Rc<xpc_object_t> {
     unsafe {
         let new_obj = xpc_string_create(CString::new(value).unwrap().into_boxed_c_str().as_ptr());
         let rc = Rc::new(new_obj);
-        let cloned = rc.clone();
-        cloned.to_owned()
+        rc
+        // let cloned = rc.clone();
+        // cloned.to_owned()
+    }
+}
+
+fn xpc_bool(value: bool) -> Rc<xpc_object_t> {
+    unsafe {
+        let new_obj = xpc_bool_create(value);
+        let rc = Rc::new(new_obj);
+        rc
+        // let cloned = rc.clone();
+        // cloned.to_owned()
     }
 }
 
@@ -46,17 +69,16 @@ fn main() {
         xpc_connection_create_mach_service(
             app_id_cstr,
             queue,
-            XPC_CONNECTION_MACH_SERVICE_PRIVILEGED as u64
+            XPC_CONNECTION_MACH_SERVICE_LISTENER as u64
         )
+        // xpc_connection_create(app_id_cstr, queue)
     };
 
-    let handler = ConcreteBlock::new(move |obj: xpc_object_t| {
-        println!("Received message!");
-        unsafe {
-            let raw_desc: *mut c_char = xpc_copy_description(obj);
-            println!("{}", CString::from_raw(raw_desc).to_str().unwrap());
-        }
-    });
+    mem::forget(app_id_cstr);
+
+    let (tx, mut rx) = unbounded::<xpc_object_t>();
+
+    let handler = ConcreteBlock::new(move |obj: xpc_object_t| tx.unbounded_send(obj));
     let handler = handler.copy();
 
     // Register handler
@@ -71,18 +93,22 @@ fn main() {
     message.insert("handle".to_string(), *xpc_i64(0));
     message.insert("subsystem".to_string(), *xpc_i64(3));
     message.insert("routine".to_string(), *xpc_i64(815));
+    message.insert("legacy".to_string(), *xpc_bool(true));
     message.insert("name".to_string(), *xpc_str("com.apple.Spotlight"));
-
-    // let mut xpc_dict_keys = message.keys()
-    //     .map(move |key| CString::new(key.deref()).unwrap().into_boxed_c_str().as_ptr())
-    //     .collect::<Vec<*const c_char>>();
-
 
     let mut xpc_dict_keys = Vec::new();
     let mut xpc_dict_values = Vec::new();
+
     for (k, v) in &message {
-        xpc_dict_keys.push(CString::new(k.deref()).unwrap().into_boxed_c_str().as_ptr());
+        unsafe {
+            let desc = CString::from_raw(xpc_copy_description(v.to_owned()));
+            println!("Adding {} {}", k, desc.to_string_lossy());
+        }
+
+        let key = CString::new(k.deref()).unwrap();
+        unsafe { xpc_dict_keys.push(key.as_ptr()) };
         xpc_dict_values.push(v.to_owned());
+        mem::forget(key);
     }
 
     let msg_dict = unsafe {
@@ -94,12 +120,21 @@ fn main() {
     };
 
     unsafe {
+        let desc = CString::from_raw(xpc_copy_description(msg_dict));
+        println!("Sending {}", desc.to_string_lossy());
         xpc_connection_send_message(connection, msg_dict);
     }
 
-    // TODO: channel
     loop {
-
+        if let Ok(Some(recv)) = rx.try_next() {
+            unsafe {
+                let desc = CString::from_raw(xpc_copy_description(recv));
+                println!("Received message! {}", desc.to_str().unwrap());
+            }
+        } else {
+            println!("Polling...nothing here");
+            std::thread::sleep(std::time::Duration::new(10, 0));
+        }
     }
 
     // let mut siv = cursive::default();
