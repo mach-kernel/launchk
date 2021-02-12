44b8024d0bc83f50eef0fff58ceb6e8ee820849b
David Stancu <dstancu@nyu.edu>
2021-02-12 15:33:55 -0500

    remove unneeded deps + cleanup

--- launchk/src/main.rs
+++ launchk/src/main.rs
@@ -5,7 +5,6 @@ use xpc_sys::*;
 use std::ffi::{CString, CStr};
 use std::os::raw::{c_char, c_void, c_int};
 use std::ptr::null_mut;
-use block::ConcreteBlock;
 use std::ops::Deref;
 use cursive::view::AnyView;
 use std::any::Any;
@@ -14,13 +13,18 @@ use std::rc::Rc;
 use std::mem;
 
 use std::sync::{mpsc, Arc};
-use futures::channel::mpsc::unbounded;
-use futures::SinkExt;
 use std::thread::sleep;
 
 extern "C" {
+    pub fn xpc_strerror(err: c_int) -> *const c_char;
     pub fn strerror(err: c_int) -> *const c_char;
     static errno: c_int;
+
+    static _os_alloc_once_table: *const _os_alloc_once_s;
+    // let global_data: *mut xpc_global_data = unsafe {
+    //     let first = _os_alloc_once_table.offset(1);
+    //     (*first).ptr as *mut _
+    // };
 }
 
 fn print_errno(err: Option<i32>) {
@@ -35,18 +39,23 @@ fn print_errno(err: Option<i32>) {
     }
 }
 
-fn main() {
+fn print_xpc_errno(err: Option<i32>) {
+    let reserr = unsafe { match err {
+        Some(e) => e,
+        None => errno,
+    } };
+
     unsafe {
-        println!("Does bootstrap port exist? {}", bootstrap_port != MACH_PORT_NULL);
+        let error = CStr::from_ptr(xpc_strerror(reserr));
+        println!("Error {}: {}", reserr, error.to_str().unwrap());
     }
+}
 
-    let found_strap_port = get_bootstrap_port();
-    let bootstrap_pipe = unsafe {
-        xpc_pipe_create_from_port(found_strap_port, 0)
-    };
+fn main() {
+    let res_bootstrap_port = get_bootstrap_port();
 
+    // "launchctl list com.apple.Spotlight"
     let mut message: HashMap<String, XPCObject> = HashMap::new();
-
     message.insert("type".to_string(), XPCObject::from(7));
     message.insert("handle".to_string(), XPCObject::from(0));
     message.insert("subsystem".to_string(), XPCObject::from(3));
@@ -58,16 +67,20 @@ fn main() {
 
     unsafe {
         let name = CString::new("domain-port").unwrap();
-        xpc_dictionary_set_mach_send(msg_dict.ptr, name.as_ptr(), bootstrap_port);
+        xpc_dictionary_set_mach_send(msg_dict.ptr, name.as_ptr(), res_bootstrap_port);
 
         let desc = CString::from_raw(xpc_copy_description(msg_dict.ptr));
-        println!("Sending {}", desc.to_string_lossy());
+        println!("Assembled message {}", desc.to_string_lossy());
     }
+
+    let pipe = unsafe { xpc_pipe_create_from_port(res_bootstrap_port, 0) };
 
     unsafe {
         let mut response: xpc_object_t = null_mut();
-        let pipe_err = xpc_pipe_routine_with_flags(bootstrap_pipe, msg_dict.ptr, &mut response, 0);
-        print_errno(Some(pipe_err));
+        let pipe_err = xpc_pipe_routine_with_flags(pipe, msg_dict.ptr, &mut response, 0);
+        print_xpc_errno(Some(pipe_err));
+        let desc = CString::from_raw(xpc_copy_description(response));
+        println!("Recv {}", desc.to_string_lossy());
     };
 
     // let mut siv = cursive::default();
--- xpc-sys/src/lib.rs
+++ xpc-sys/src/lib.rs
@@ -2,22 +2,27 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::os::raw::{c_void, c_char, c_int};
+use std::os::raw::{c_void, c_char, c_int, c_long, c_uint};
 use std::sync::Arc;
 use std::ffi::CString;
 use std::collections::HashMap;
 use std::mem;
 use std::ops::Deref;
 use std::iter::FromIterator;
+use std::ptr::{null_mut};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+type xpc_pipe_t = *mut c_void;
+
 /**
  * Some extra private API definitions. Thanks:
+ *
+ * https://developer.apple.com/documentation/kernel/mach
  * https://chromium.googlesource.com/chromium/src.git/+/47.0.2507.2/sandbox/mac/xpc_private_stubs.sig
  */
 extern "C" {
-    pub fn xpc_pipe_create_from_port(port: mach_port_t, flags: u64) -> *mut c_void;
+    pub fn xpc_pipe_create_from_port(port: mach_port_t, flags: u64) -> xpc_pipe_t;
     pub fn xpc_pipe_routine_with_flags(pipe: *mut c_void, msg: xpc_object_t, response: *mut xpc_object_t, flags: u64) -> c_int;
     pub fn xpc_dictionary_set_mach_send(object: xpc_object_t, name: *const c_char, port: mach_port_t);
 }
@@ -27,11 +32,25 @@ pub struct XPCObject {
     pub ptr: xpc_object_t,
 }
 
+#[repr(C)]
+pub struct _os_alloc_once_s {
+    pub once: c_long,
+    pub ptr: *mut c_void,
+}
+
+#[repr(C)]
+pub struct xpc_global_data {
+    pub a: u_int64_t,
+    pub xpc_flags: u_int64_t,
+    pub task_bootstrap_port: mach_port_t,
+    pub xpc_bootstrap_pipe: xpc_pipe_t
+}
+
 impl From<i64> for XPCObject {
     /// Create XPCObject via xpc_int64_create
     fn from(value: i64) -> Self {
         unsafe {
-            return XPCObject { ptr: xpc_int64_create(value) };
+            XPCObject { ptr: xpc_int64_create(value) }
         }
     }
 }
@@ -40,7 +59,7 @@ impl From<bool> for XPCObject {
     /// Create XPCObject via xpc_bool_create
     fn from(value: bool) -> Self {
         unsafe {
-            return XPCObject { ptr: xpc_bool_create(value) };
+            XPCObject { ptr: xpc_bool_create(value) }
         }
     }
 }
@@ -69,33 +88,68 @@ impl From<HashMap<String, XPCObject>> for XPCObject {
         let mut obj_values = Vec::from_iter(message.values().map(|xo| xo.ptr));
 
         unsafe {
-            return XPCObject {
+            XPCObject {
                 ptr: xpc_dictionary_create(
                     cstr_ptrs.as_ptr(),
                     obj_values.as_mut_ptr(),
                     message.len() as u64
                 )
-            };
+            }
         }
     }
 }
 
 /// Look up bootstrap port for mach_task_self
-pub fn get_bootstrap_port() -> mach_port_t {
+pub fn lookup_bootstrap_port() -> mach_port_t {
     let mut num_ports: mach_msg_type_number_t = 0;
-    let mut ret_ports: *mut mach_port_t = null_mut();
+    let mut found_ports: *mut mach_port_t = null_mut();
 
-    unsafe {
-        mach_ports_lookup(mach_task_self_, &mut ret_ports as *mut _, &mut num_ports);
+    let kr: kern_return_t = unsafe {
+        mach_ports_lookup(mach_task_self_, &mut found_ports, &mut num_ports)
+    };
+
+    if kr != KERN_SUCCESS as i32 {
+        panic!("Unable to obtain Mach bootstrap port!");
     }
 
-    println!("Found {} ports for mach_task_self_", num_ports);
+    let mut ret_port: mach_port_t = unsafe { *found_ports.offset(0) };
+    println!("{} ports for mach_task_self_, taking first: mach_port_t {}", num_ports, ret_port);
 
+    // Deallocate others
     unsafe {
-        println!("Returning mach_port_t {}", *ret_ports);
-        return *ret_ports;
+        for i in 1..num_ports {
+            let port = *found_ports.offset(i as isize);
+            println!("Deallocating mach_port_t {}", port);
+            mach_port_deallocate(mach_task_self_, port);
+        }
+    }
+
+    ret_port
+}
+
+/// Attempt to yield existing bootstrap_port if not
+/// MACH_PORT_NULL
+pub fn get_bootstrap_port() -> mach_port_t {
+    unsafe {
+        match bootstrap_port {
+            MACH_PORT_NULL => {
+                println!("Bootstrap port is null! Querying for port");
+                lookup_bootstrap_port()
+            },
+            _ => {
+                println!("Found bootstrap port {}", bootstrap_port);
+                bootstrap_port
+            }
+        }
     }
 }
+
+// pub fn get_xpc_global_data() -> Option<xpc_global_data> {
+//     let global_data: *mut xpc_global_data = unsafe {
+//         let first = _os_alloc_once_table.offset(1);
+//         (*first).ptr as *mut _
+//     };
+// }
 
 #[cfg(test)]
 mod tests {
