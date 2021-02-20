a33ebf4d4320c54077de1774a387692ccae53e8d
David Stancu <dstancu@nyu.edu>
2021-02-20 14:41:10 -0500

    getting response from launchd!

--- launchk/src/main.rs
+++ launchk/src/main.rs
@@ -1,58 +1,58 @@
 use cursive::views::{Dialog, TextView};
 
-use xpc_sys;
-use xpc_sys::*;
-use std::ffi::{CString, CStr};
-use std::os::raw::{c_char, c_void, c_int};
-use std::ptr::null_mut;
-use std::ops::Deref;
 use cursive::view::AnyView;
 use std::any::Any;
 use std::collections::HashMap;
-use std::rc::Rc;
+use std::ffi::{CStr, CString};
 use std::mem;
+use std::ops::Deref;
+use std::os::raw::{c_char, c_int, c_void};
+use std::ptr::null_mut;
+use std::rc::Rc;
+use xpc_sys;
+use xpc_sys::*;
 
 use std::sync::{mpsc, Arc};
 use std::thread::sleep;
 
 fn main() {
-    let res_bootstrap_port = get_bootstrap_port();
-
     // "launchctl list com.apple.Spotlight"
-    let mut message: HashMap<String, XPCObject> = HashMap::new();
-    message.insert("type".to_string(), XPCObject::from(7));
-    message.insert("handle".to_string(), XPCObject::from(0));
-    message.insert("subsystem".to_string(), XPCObject::from(3));
-    message.insert("routine".to_string(), XPCObject::from(815));
-    message.insert("legacy".to_string(), XPCObject::from(true));
-    message.insert("name".to_string(), XPCObject::from("com.apple.Spotlight"));
+    let mut message: HashMap<&str, XPCObject> = HashMap::new();
+    message.insert("type", XPCObject::from(1 as u64));
+    message.insert("handle", XPCObject::from(0 as u64));
+    message.insert("subsystem", XPCObject::from(3 as u64));
+    message.insert("routine", XPCObject::from(815 as u64));
+    message.insert("legacy", XPCObject::from(true));
+    message.insert("name", XPCObject::from("com.apple.Spotlight"));
+    message.insert(
+        "domain-port",
+        XPCObject::from(get_bootstrap_port() as mach_port_t),
+    );
 
     let msg_dict = XPCObject::from(message);
-
-    unsafe {
-        let name = CString::new("domain-port").unwrap();
-        xpc_dictionary_set_mach_send(msg_dict.ptr, name.as_ptr(), res_bootstrap_port);
-
-        let desc = CString::from_raw(xpc_copy_description(msg_dict.ptr));
-        println!("Assembled message {}", desc.to_string_lossy());
-    }
+    println!("Sending {}", msg_dict);
 
     let pipe = get_xpc_bootstrap_pipe();
-    unsafe {
+    let response = unsafe {
         let mut response: xpc_object_t = null_mut();
-        let pipe_err = xpc_pipe_routine_with_flags(pipe, msg_dict.ptr, &mut response, 0);
-        print_errno(Some(pipe_err));
-        let desc = CString::from_raw(xpc_copy_description(response));
-        println!("Recv {}", desc.to_string_lossy());
+        let err = xpc_pipe_routine_with_flags(pipe, msg_dict.data, &mut response, 0);
+
+        if err != 0 {
+            print_errno(Some(err));
+            panic!("Could not send!")
+        }
+
+        response
     };
 
+    println!("Received {}", XPCObject::from(response));
+
     // let mut siv = cursive::default();
-    //
-    // // Creates a dialog with a single "Quit" button
-    // siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
-    //                      .title("Cursive")
-    //                      .button("Quit", |s| s.quit()));
-    //
-    // // Starts the event loop.
+    // Creates a dialog with a single "Quit" button
+    // siv.add_layer(Dialog::around(TextView::new(recv))
+    //     .title("test")
+    //     .button("Quit", |s| s.quit()));
+
+    // Starts the event loop.
     // siv.run();
 }
--- xpc-sys/src/lib.rs
+++ xpc-sys/src/lib.rs
@@ -2,14 +2,14 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-use std::os::raw::{c_void, c_char, c_int, c_long, c_uint};
-use std::sync::Arc;
-use std::ffi::{CString, CStr};
 use std::collections::HashMap;
-use std::mem;
-use std::ops::Deref;
+use std::ffi::{CStr, CString};
 use std::iter::FromIterator;
-use std::ptr::{null_mut, null};
+use std::ops::Deref;
+use std::os::raw::{c_char, c_int, c_long, c_uint, c_void};
+use std::ptr::{null, null_mut};
+use std::sync::Arc;
+use std::{fmt, mem};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -23,24 +23,35 @@ type xpc_pipe_t = *mut c_void;
  */
 extern "C" {
     // Seems to give same output as strerror ¯\_(ツ)_/¯
-    pub fn xpc_strerror(err: c_int) ->  *const c_char;
+    pub fn xpc_strerror(err: c_int) -> *const c_char;
 
     pub static errno: c_int;
 
     pub fn xpc_pipe_create_from_port(port: mach_port_t, flags: u64) -> xpc_pipe_t;
-    pub fn xpc_pipe_routine_with_flags(pipe: *mut c_void, msg: xpc_object_t, response: *mut xpc_object_t, flags: u64) -> c_int;
-    pub fn xpc_dictionary_set_mach_send(object: xpc_object_t, name: *const c_char, port: mach_port_t);
+    pub fn xpc_pipe_routine_with_flags(
+        pipe: xpc_pipe_t,
+        msg: xpc_object_t,
+        reply: *mut xpc_object_t,
+        flags: u64,
+    ) -> c_int;
+    pub fn xpc_pipe_routine(pipe: xpc_pipe_t, msg: xpc_object_t, reply: *mut xpc_object_t)
+        -> c_int;
+
+    pub fn xpc_mach_send_create(port: mach_port_t) -> xpc_object_t;
+    pub fn xpc_dictionary_set_mach_send(
+        object: xpc_object_t,
+        name: *const c_char,
+        port: mach_port_t,
+    );
 
     // https://opensource.apple.com/source/Libsystem/Libsystem-1213/alloc_once_private.h.auto.html
     pub static _os_alloc_once_table: [_os_alloc_once_s; 10];
-
-    // TODO: why doesn't this work?
-    // pub static _os_alloc_once_table: *const _os_alloc_once_s;
 }
 
 /// newtype for xpc_object_t
+#[repr(C)]
 pub struct XPCObject {
-    pub ptr: xpc_object_t,
+    pub data: xpc_object_t,
 }
 
 #[repr(C)]
@@ -54,14 +65,52 @@ pub struct xpc_global_data {
     pub a: u_int64_t,
     pub xpc_flags: u_int64_t,
     pub task_bootstrap_port: mach_port_t,
-    pub xpc_bootstrap_pipe: xpc_pipe_t
+    pub xpc_bootstrap_pipe: xpc_pipe_t,
+}
+
+impl fmt::Display for XPCObject {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let xpc_desc = unsafe { xpc_copy_description(self.data) };
+        let cstr = unsafe { CStr::from_ptr(xpc_desc) };
+        write!(f, "{}", cstr.to_string_lossy())
+    }
+}
+
+impl From<xpc_object_t> for XPCObject {
+    fn from(value: xpc_object_t) -> Self {
+        XPCObject { data: value }
+    }
 }
 
 impl From<i64> for XPCObject {
     /// Create XPCObject via xpc_int64_create
     fn from(value: i64) -> Self {
         unsafe {
-            XPCObject { ptr: xpc_int64_create(value) }
+            XPCObject {
+                data: xpc_int64_create(value),
+            }
+        }
+    }
+}
+
+impl From<u64> for XPCObject {
+    /// Create XPCObject via xpc_uint64_create
+    fn from(value: u64) -> Self {
+        unsafe {
+            XPCObject {
+                data: xpc_uint64_create(value),
+            }
+        }
+    }
+}
+
+impl From<mach_port_t> for XPCObject {
+    /// Create XPCObject via xpc_uint64_create
+    fn from(value: mach_port_t) -> Self {
+        unsafe {
+            XPCObject {
+                data: xpc_mach_send_create(value),
+            }
         }
     }
 }
@@ -70,7 +119,9 @@ impl From<bool> for XPCObject {
     /// Create XPCObject via xpc_bool_create
     fn from(value: bool) -> Self {
         unsafe {
-            XPCObject { ptr: xpc_bool_create(value) }
+            XPCObject {
+                data: xpc_bool_create(value),
+            }
         }
     }
 }
@@ -81,32 +132,25 @@ impl From<&str> for XPCObject {
         let cstr = CString::new(slice).unwrap();
         unsafe {
             return XPCObject {
-                ptr: xpc_string_create(cstr.into_boxed_c_str().as_ptr())
-            }
+                data: xpc_string_create(cstr.into_boxed_c_str().as_ptr()),
+            };
         }
     }
 }
 
-impl From<HashMap<String, XPCObject>> for XPCObject {
+impl From<HashMap<&str, XPCObject>> for XPCObject {
     /// Create an XPC dictionary from HashMap<String, XPCObject>
-    fn from(message: HashMap<String, XPCObject>) -> Self {
-        // Must have valid references to the CStrings else they get dropped
-        // before we can load them using xpc_dictionary_create
-        let cstr_keys = Vec::from_iter(
-            message.keys().flat_map(|k| CString::new(k.to_string()))
-        );
-        let cstr_ptrs = Vec::from_iter(cstr_keys.iter().map(|cs| cs.as_ptr()));
-        let mut obj_values = Vec::from_iter(message.values().map(|xo| xo.ptr));
+    fn from(message: HashMap<&str, XPCObject>) -> Self {
+        let dict = unsafe { xpc_dictionary_create(null_mut(), null_mut(), 0) };
 
-        unsafe {
-            XPCObject {
-                ptr: xpc_dictionary_create(
-                    cstr_ptrs.as_ptr(),
-                    obj_values.as_mut_ptr(),
-                    message.len() as u64
-                )
+        for (k, v) in message {
+            unsafe {
+                let cstr = CString::new(k);
+                xpc_dictionary_set_value(dict, cstr.unwrap().as_ptr(), v.data);
             }
         }
+
+        XPCObject { data: dict }
     }
 }
 
@@ -115,16 +159,18 @@ pub fn lookup_bootstrap_port() -> mach_port_t {
     let mut num_ports: mach_msg_type_number_t = 0;
     let mut found_ports: *mut mach_port_t = null_mut();
 
-    let kr: kern_return_t = unsafe {
-        mach_ports_lookup(mach_task_self_, &mut found_ports, &mut num_ports)
-    };
+    let kr: kern_return_t =
+        unsafe { mach_ports_lookup(mach_task_self_, &mut found_ports, &mut num_ports) };
 
     if kr != KERN_SUCCESS as i32 {
         panic!("Unable to obtain Mach bootstrap port!");
     }
 
     let mut ret_port: mach_port_t = unsafe { *found_ports.offset(0) };
-    println!("{} ports for mach_task_self_, taking first: mach_port_t {}", num_ports, ret_port);
+    println!(
+        "{} ports for mach_task_self_, taking first: mach_port_t {}",
+        num_ports, ret_port
+    );
 
     // Deallocate others
     unsafe {
@@ -157,15 +203,18 @@ pub fn get_xpc_bootstrap_pipe() -> xpc_pipe_t {
     match read_xpc_global_data() {
         Some(xpcgd) => {
             unsafe {
-                println!("Found _os_alloc_once_table: {:?}", &_os_alloc_once_table as *const _);
+                println!(
+                    "Found _os_alloc_once_table: {:?}",
+                    &_os_alloc_once_table as *const _
+                );
                 println!("Found xpc_bootstrap_pipe: {:?}", xpcgd.xpc_bootstrap_pipe);
             }
             xpcgd.xpc_bootstrap_pipe
-        },
+        }
         None => unsafe {
             println!("Can't find _os_alloc_once_table, creating new bootstrap pipe");
             xpc_pipe_create_from_port(get_bootstrap_port(), 0)
-        }
+        },
     }
 }
 
