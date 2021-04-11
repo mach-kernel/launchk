9a583576b0195657bb4dccebfec08c807e24f87c
David Stancu <dstancu@nyu.edu>
2021-04-11 12:09:23 -0400

    [2/3] very horrifying way of listing all

--- launchk/src/launchd/service.rs
+++ launchk/src/launchd/service.rs
@@ -1,5 +1,5 @@
 use crate::launchd::message::{from_msg, LIST_SERVICES};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::convert::{TryFrom, TryInto};
 use std::fmt;
 use std::path::Path;
@@ -12,6 +12,7 @@ use xpc_sys::traits::xpc_value::TryXPCValue;
 use xpc_sys::objects::xpc_dictionary::XPCDictionary;
 use xpc_sys::objects::xpc_error::XPCError;
 use crate::launchd::config::{LaunchdEntryType, LaunchdEntryLocation, USER_LAUNCH_AGENTS, ADMIN_LAUNCH_DAEMONS, ADMIN_LAUNCH_AGENTS, SYSTEM_LAUNCH_DAEMONS, SYSTEM_LAUNCH_AGENTS, LaunchdEntryConfig};
+use std::iter::FromIterator;
 
 lazy_static! {
     static ref ENTRY_INFO_CACHE: Mutex<HashMap<String, LaunchdEntryInfo>> =
@@ -75,6 +76,38 @@ pub struct LaunchdEntryInfo {
     pub limit_load_to_session_type: LimitLoadToSessionType,
 }
 
+pub fn list(domain_type: u64, name: Option<String>) -> Result<XPCDictionary, XPCError> {
+    let mut msg = from_msg(&LIST_SERVICES);
+    msg.insert("type", domain_type.into());
+
+    if name.is_some() {
+        msg.insert("name", name.unwrap().into());
+    }
+
+    let msg: XPCObject = msg.into();
+    msg
+        .pipe_routine()
+        .and_then(|o| o.try_into())
+        .and_then(|dict: XPCDictionary| dict.0.get("error").map(|e| {
+            Err(XPCError::PipeError(e.to_string()))
+        }).unwrap_or(Ok(dict)))
+}
+
+pub fn list_all() -> HashSet<String> {
+    let everything = (0..8)
+        .filter_map(|t| {
+            let svc_for_type = list(t as u64, None)
+                .and_then(|d| d.get_as_dictionary(&["services"]))
+                .map(|XPCDictionary(ref hm)| hm.keys().map(|k| k.clone()).collect());
+
+            svc_for_type.ok()
+        })
+        .flat_map(|k: Vec<String>| k.into_iter());
+
+    HashSet::from_iter(everything)
+}
+
+/// Get more information about a unit from its label
 pub fn find_entry_info<T: Into<String>>(label: T) -> LaunchdEntryInfo {
     let label_string = label.into();
     let mut cache = ENTRY_INFO_CACHE.try_lock().unwrap();
@@ -90,15 +123,9 @@ pub fn find_entry_info<T: Into<String>>(label: T) -> LaunchdEntryInfo {
 fn build_entry_info<T: Into<String>>(label: T) -> LaunchdEntryInfo {
     let label_string = label.into();
 
-    // "launchctl list [name]"
-    let mut msg: HashMap<&str, XPCObject> = from_msg(&LIST_SERVICES);
-    msg.insert("name", label_string.clone().into());
-
-    let msg: XPCObject = msg.into();
-    let limit_load_to_session_type: LimitLoadToSessionType = msg
-        .pipe_routine()
-        .and_then(|o| o.try_into())
-        .and_then(|d: XPCDictionary| d.get(&["service", "LimitLoadToSessionType"]))
+    // "launchctl list [name]", type is 7 when name provided?
+    let limit_load_to_session_type = list(7 as u64, Some(label_string.clone()))
+        .and_then(|d| d.get(&["service", "LimitLoadToSessionType"]))
         .and_then(|r| r.try_into())
         .unwrap_or(LimitLoadToSessionType::Unknown);
 
