0428bfefa6a2fe67a3ad3d01768c6524f79e38df
David Stancu <dstancu@barracuda.com>
2021-04-26 17:42:44 -0400

    impl viewwrapper for omniboxsubscribedview instead of view

--- launchk/src/tui/omnibox/subscribed_view.rs
+++ launchk/src/tui/omnibox/subscribed_view.rs
@@ -11,45 +11,18 @@ pub struct OmniboxSubscribedView {
     inner: Box<dyn OmniboxSubscriber>,
 }
 
-impl View for OmniboxSubscribedView {
-    fn draw(&self, printer: &Printer<'_, '_>) {
-        self.inner.draw(printer)
+/// Implement ViewWrapper to forward to inner
+impl ViewWrapper for OmniboxSubscribedView {
+    type V = dyn OmniboxSubscriber;
+
+    fn with_view<F, R>(&self, f: F) -> Option<R> where
+        F: FnOnce(&Self::V) -> R {
+        Some(f(&*self.inner))
     }
 
-    fn layout(&mut self, size: Vec2) {
-        self.inner.layout(size)
-    }
-
-    fn needs_relayout(&self) -> bool {
-        self.inner.needs_relayout()
-    }
-
-    fn required_size(&mut self, constraint: Vec2) -> Vec2 {
-        self.inner.required_size(constraint)
-    }
-
-    fn on_event(&mut self, event: Event) -> EventResult {
-        self.inner.on_event(event)
-    }
-
-    fn call_on_any<'a>(&mut self, sel: &Selector<'_>, f: &'a mut dyn FnMut(&mut dyn View)) {
-        self.inner.call_on_any(sel, f)
-    }
-
-    fn focus_view(&mut self, sel: &Selector<'_>) -> Result<(), ()> {
-        self.inner.focus_view(sel)
-    }
-
-    fn take_focus(&mut self, source: Direction) -> bool {
-        self.inner.take_focus(source)
-    }
-
-    fn important_area(&self, view_size: Vec2) -> Rect {
-        self.inner.important_area(view_size)
-    }
-
-    fn type_name(&self) -> &'static str {
-        self.inner.type_name()
+    fn with_view_mut<F, R>(&mut self, f: F) -> Option<R> where
+        F: FnOnce(&mut Self::V) -> R {
+        Some(f(&mut *self.inner))
     }
 }
 
